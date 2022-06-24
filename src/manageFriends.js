9340a1fa936ab6e917cd3e3ab40bd4491b453163
Joe <hutcheson2001@yahoo.com>
2022-06-24 14:31:20 -0400

    commit tree 13a1b4cf169a559b00ba757eb726906ba025317e

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,11 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: [] };
+
+export function manageFriends(state=initialState, action) {
+  if (action.type === "friends/add"){
+
+    return {state.friends.push(action.payload)}
+  } else if (action.type === "friends/remove"){
+    return {friends:state.friends.filter((friend)=>friend.id!==action.payload)}
+  }
+  return state
 }
